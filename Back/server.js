1decf4915c39b13084ee30e238a6f3ee39d9e6ca
Your Name <you@example.com>
2025-12-02 15:23:20 +0100

    commit 12

--- Back/server.js
+++ Back/server.js
@@ -1,13 +1,16 @@
 //chargement du module expresse qui permet de créer un serveur web
 //et de gérer les requêtes HTTP
 const express = require('express');
+const path = require('path');
 
 //création d'une instance de l'application express
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "Front")));
+
 //définition d'une route get pour la racine ('/')
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.sendFile(path.join(__dirname, "..", "Front", "index.html"));
 });
 
 //Listen attends 2 paramètres : le port d'écoute et une fonction anonyme de rappel
